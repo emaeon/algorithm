1dc46800e70632bd7789b1034f5cd904a0127773
emaeon <gun9809@naver.com>
2024-04-30 15:33:50 +0900

    6

--- 3.반복문/코딩은체육과목입니다.py
+++ 3.반복문/코딩은체육과목입니다.py
@@ -5,10 +5,10 @@ input=sys.stdin.readline
 N=int(input())
 
 A = N//4
-# name = A*str('long')
+# name = A*str('long ')
 
 a=''
 for _ in range(A):
    a += str('long ')
 
-print(a+'int',end="") #,는 자동으로 공백이 생김, 따라서 +로
+print(name+'int',end="") #,는 자동으로 공백이 생김, 따라서 +로
