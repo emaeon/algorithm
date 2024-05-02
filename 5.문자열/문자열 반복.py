cce4bc7746d0ee110bb78c1df992a4006e01f7d8
emaeon <gun9809@naver.com>
2024-05-02 12:22:52 +0900

    7

---
+++ 5.문자열/문자열 반복.py
@@ -0,0 +1,17 @@
+import sys
+
+input=sys.stdin.readline
+
+t=int(input().rstrip())
+
+
+for _ in range(t):
+    r,s=map(str,input().split())
+    string=''
+    for i in s:
+        string+=int(r)*str(i)
+    print(string)
+        
+
+
+        
