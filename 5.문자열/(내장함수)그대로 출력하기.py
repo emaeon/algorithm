eee64c2f41a42907627cd26de61822d683401959
emaeon <gun9809@naver.com>
2024-05-03 12:14:27 +0900

    11

--- 5.문자열/(내장함수)그대로 출력하기.py
+++ 5.문자열/(내장함수)그대로 출력하기.py
@@ -2,9 +2,16 @@ import sys
 
 input=sys.stdin.readline
 
-while True:
+for _ in range(100):
+    
     try:
         print(input().rstrip())
         
     except:
         break
+    
+# while True:
+#     try:
+#         print(input())
+#     except EOFError: #End Of File Error 입력값이 없는 경우
+#         break
