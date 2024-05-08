1f94708cf4064e56e5d1bae667dcb8b6f3384ff5
emaeon <gun9809@naver.com>
2024-05-08 20:39:26 +0900

    3

--- 7. 2차원 배열/(다른풀이가있음)세로읽기.py
+++ 7. 2차원 배열/(다른풀이가있음)세로읽기.py
@@ -2,7 +2,7 @@ import sys
 
 input=sys.stdin.readline
 #각 차원 별 인덱스 0 저장 -> 인덱스 1 저장 -> 인덱스 3 저장 ...
-
+#만약 out of index면? 무시하고 그다음으로 
 lst=[]
 wordlen=0
 for _ in range(5):
@@ -21,4 +21,3 @@ for i in range(wordlen):
         
         
 print(string)
-    
