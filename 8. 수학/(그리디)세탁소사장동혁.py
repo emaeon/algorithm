4ce6105a448275d86128ea412a6b4d60c7e32418
emaeon <gun9809@naver.com>
2024-05-10 14:25:41 +0900

    3

---
+++ 8. 수학/(그리디)세탁소사장동혁.py
@@ -0,0 +1,16 @@
+import sys
+
+input=sys.stdin.readline
+
+t=int(input().rstrip())
+remain=[25,10,5,1]
+
+
+for _ in range(t):
+    price=int(input().rstrip())    
+    temp_lst=[]
+    for i in remain:
+        temp_lst.append(price//i)
+        price = price%i
+    print(*temp_lst)
+    
