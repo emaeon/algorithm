3e037b7f296018febc0726639b4e9558ba38c105
emaeon <gun9809@naver.com>
2024-05-17 16:21:28 +0900

    5

--- 10. 기하 직사각형과 삼각형/대지.py
+++ 10. 기하 직사각형과 삼각형/대지.py
@@ -2,7 +2,26 @@ import sys
 
 input=sys.stdin.readline
 
+# 규칙
+# x에서 제일 큰 수 - 제일 작은 수
+# y에서 제일 큰 수 - 제일 작은 수
+# 두 값을 곱 
 
-a=int(input().rstrip())
+n = int(input().rstrip())
 
-print(4*a)
+lstx=[]
+lsty=[]
+for _ in range(n):
+    x,y = map(int,input().split())
+    lstx.append(x)
+    lsty.append(y)
+    
+if len(lstx) == 1:
+    print(0)
+    
+else:
+    w = max(lstx)-min(lstx)
+    h = max(lsty)-min(lsty)
+
+    print(w*h)
+    
