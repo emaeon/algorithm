e09ccc4658cc2af81ad18d924a401af14a6b3de3
emaeon <gun9809@naver.com>
2024-08-20 22:48:45 +0900

    스택2 틀림

---
+++ 코딩테스트 스터디/스택,큐/요세푸스.py
@@ -0,0 +1,20 @@
+from collections import deque
+import sys
+
+queue = deque()
+input = sys.stdin.readline
+n,k = map(int, input().split())
+
+for i in range(n): # 1번부터 N번까지 N명의 사람
+    queue.append(i+1)
+
+result=[]
+while queue : #큐 안에 사람들 모두 사라질 때까지 반복
+    for _ in range(k-1): #K번째 사람전까지
+        queue.append(queue.popleft())  #맨 앞에 있던 사람을 맨 뒤로 옮기기
+        
+    result.append(queue.popleft()) #K번째 사람 큐에서 제거하고 result에 그 사람 넣기
+
+print('<',end='')
+print(*result, sep=', ', end ='')
+print('>',end='')
