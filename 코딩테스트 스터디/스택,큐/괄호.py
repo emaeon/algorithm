7729035a110077a561e8030201f8a03b3a0b4898
emaeon <gun9809@naver.com>
2024-08-22 20:39:17 +0900

    스택4 맞춤

---
+++ 코딩테스트 스터디/스택,큐/괄호.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input().rstrip())
+
+for _ in range(n):
+    a = list(input().rstrip())
+
+    if a[0] ==")":
+        print("NO")
+    
+    elif a[::-1]=="(":
+        print("NO")
+    
+    elif a.count("(") != a.count(')'):
+        print("NO")
+        
+    elif a[0]=="(":
+        
+        cnt1, cnt2 = 0, 0
+        for i in a :
+            if i == '(':
+                cnt1 += 1
+            if i == ')' :
+                cnt2 += 1
+            if cnt2 > cnt1 :
+                print('NO')
+                break
+        
+        if cnt1 == cnt2:
+            print('YES')
+
+            
+        
+                
+
