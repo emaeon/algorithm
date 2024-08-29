9e68f1aef966da225422f68869e18fae9d1e49bd
emaeon <gun9809@naver.com>
2024-08-29 15:41:58 +0900

    틀림

--- 코딩테스트 스터디/DFS,BFS/연결 요소의 개수.py
+++ 코딩테스트 스터디/DFS,BFS/연결 요소의 개수.py
@@ -1,47 +1,35 @@
 import sys
-from collections import deque
-
-
-def dfs(idx):
-    global visited
-    visited[idx] = True
-    print(idx, end=' ')
-    
-    for i in range(1, n+1):
-        if not visited[i] and graph[idx][i]:
-            dfs(i)
-
-def bfs():
-    global queue, visited
-    while queue :
-        cur = queue.popleft()
-        visited[cur] = True
-        print(cur, end=' ')
-        
-        for i in range(1, n+1):
-            if not visited[i] and graph[cur][i]:
-                visited[i] = True
-                queue.append(i)
-            
-    
-#0. input 받기
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-n, m, v = map(int, input().split())
 
+#그래프
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
 
-#1. graph 및 visited 배열 선언
-graph=[[False]*(n+1) for _ in range(n+1)]
-dd
-for _ in range(m):
-    a, b = map(int,input().split())
-    graph[a][b] = True
-    graph[b][a] = True
-# print(graph)
-visited= [False]*(n+1)
+for _ in range(m) :
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-dfs(v)
-print()
+#visitied
+visited = [False] *(n+1)
 
-visited = [False]*(n+1)
-queue = deque([v])
-bfs()
+#dfs
+def dfs(v):
+    visited[v]=True
+    # print(v, end = ' ')
+    
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+            
+            
+count = 0
+
+for i in range(1, n+1):
+    if not visited[i]:
+        dfs(i)
+        count+=1
+#dfs 실행
+
+print(count)
---
+++ 코딩테스트 스터디/DFS,BFS/유기농배추.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+
+
+def dfs(idx):
+    global visited
+    visited[idx] = True
+    print(idx, end=' ')
+    
+    for i in range(1, n+1):
+        if not visited[i] and graph[idx][i]:
+            dfs(i)
+
+def bfs():
+    global queue, visited
+    while queue :
+        cur = queue.popleft()
+        visited[cur] = True
+        print(cur, end=' ')
+        
+        for i in range(1, n+1):
+            if not visited[i] and graph[cur][i]:
+                visited[i] = True
+                queue.append(i)
+            
+    
+#0. input 받기
+input = sys.stdin.readline
+n, m, v = map(int, input().split())
+
+
+#1. graph 및 visited 배열 선언
+graph=[[False]*(n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int,input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+# print(graph)
+visited= [False]*(n+1)
+
+dfs(v)
+print()
+
+visited = [False]*(n+1)
+queue = deque([v])
+bfs()
