455a99d105c357493c7169f79c16f71ed12ab605
emaeon <gun9809@naver.com>
2024-09-07 12:39:42 +0900

    algorithm

--- 코딩테스트 스터디/백트래킹/암호 만들기.py
+++ 코딩테스트 스터디/백트래킹/암호 만들기.py
@@ -1,27 +1,34 @@
 import sys
 
-L,C= map(int,input().split())
 
-lst = list(map(str,input().split()))
-lst.sort()
+def back_tracking(cnt, idx):
 
-result = []
-visited = [False]*C
+    # 암호를 만들었을 때
+    if cnt == l:
+        # 모음, 자음 체크
+        vo, co = 0, 0
 
-def back():
-    
-    if len(result) ==  L :
-        print(*result, sep='')
+        for i in range(l):
+            if answer[i] in consonant:
+                vo += 1
+            else:
+                co += 1
+
+        # 모음 1개 이상, 자음 2개 이상
+        if vo >= 1 and co >= 2:
+            print("".join(answer))
+
         return
-    temp = ''
-    for i in range(C) :
-        if temp != lst[i] and not visited[i]: 
-            
-            visited[i] = True
-            result.append(lst[i])
-            temp = lst[i]
-            back()
-            visited[i]= False
-            result.pop()
-            
-back()        
+
+    # 반복문을 통해 암호를 만든다.
+    for i in range(idx, c):
+        answer.append(words[i])
+        back_tracking(cnt + 1, i + 1) # 백트래킹
+        answer.pop()
+
+
+l, c = map(int, sys.stdin.readline().split())
+words = sorted(list(map(str, sys.stdin.readline().split())))
+consonant = ['a', 'e', 'i', 'o', 'u']
+answer = []
+back_tracking(0, 0)
