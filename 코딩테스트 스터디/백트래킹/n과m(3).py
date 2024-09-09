6990d5d4720257b6fdb659d80fdaaa70b2f4871d
emaeon <gun9809@naver.com>
2024-09-09 16:18:11 +0900

    day1

--- 코딩테스트 스터디/백트래킹/n과m(3).py
+++ 코딩테스트 스터디/백트래킹/n과m(3).py
@@ -3,12 +3,12 @@ n,m= map(int,input().split())
 s = []
  
 def dfs():
-    if len(s)==m:
+    if len(s)==m: #m=2
         print(' '.join(map(str,s)))
         return
     
-    for i in range(1,n+1):
+    for i in range(1,n+1):  #[1, 1] -> == m과 길이 -> [1]-> [1,2] 
         s.append(i)
         dfs()
-        s.pop()
+        s.pop()#길이 같아지면 return하고 실행되니 빠짐
 dfs()
--- 코딩테스트 스터디/백트래킹/암호 만들기.py
+++ 코딩테스트 스터디/백트래킹/암호 만들기.py
@@ -8,7 +8,7 @@ def back_tracking(cnt, idx):
         # 모음, 자음 체크
         vo, co = 0, 0
 
-        for i in range(l): # 
+        for i in range(l): #암호문길이
             if answer[i] in consonant: #모음이 있으면
                 vo += 1 # 모음 추가
             else:
@@ -21,10 +21,10 @@ def back_tracking(cnt, idx):
         return
 
     # 반복문을 통해 암호를 만든다.
-    for i in range(idx, c):
-        answer.append(words[i])
-        back_tracking(cnt + 1, i + 1) # 백트래킹
-        answer.pop()
+    for i in range(idx, c): # 1. range(0, 문자 길이) 2.range(1,문자길이)
+        answer.append(words[i]) # 1. answer=['word[0]'], 2.answer=['word[0]','word[1]']
+        back_tracking(cnt + 1, i + 1) # 1. back(1,1) #
+        answer.pop() #1. answer
 
 
 l, c = map(int, sys.stdin.readline().split())
