b72928e543ad011c27c80fcc13924b3896905197
emaeon <gun9809@naver.com>
2024-09-19 23:45:06 +0900

    day1

--- 알고리즘고득점kit/완전탐색/모의고사.py
+++ 알고리즘고득점kit/완전탐색/모의고사.py
@@ -1,18 +1,23 @@
 def solution(answers):
-	pattern1 = [1,2,3,4,5]
-    pattern2 = [2,1,2,3,2,4,2,5]
-    pattern3 = [3,3,1,1,2,2,4,4,5,5]
-    score = [0,0,0]
-    resullt = []
+    one = [1,2,3,4,5] #1번 수포자 패턴
+    two = [2,1,2,3,2,4,2,5] #2번 수포자 패턴
+    three = [3,3,1,1,2,2,4,4,5,5] #3번 수포자 패턴
     
-    for idx, answer in enumerate(answers):
-    	if answer == pattern1[idx%len(pattern1)]:
-        	score[0] += 1
-    	if answer == pattern2[idx%len(pattern2)]:
-        	score[1] += 1
-    	if answer == pattern3[idx%len(pattern3)]:
-        	score[2] += 1   
-    for idx, s in enumerate(score):
-    	if s == max(score):
-        	result.append(idx+1)
-    return result
+    # 수포자 정답 체크
+    ans = [0, 0, 0]
+    
+    for i in range(len(answers)):
+        if answers[i] == one[i % len(one)]: 
+            ans[0] += 1
+        if answers[i] == two[i % len(two)]:
+            ans[1] += 1
+        if answers[i] == three[i % len(three)]:
+            ans[2] += 1
+           
+    # 체크한 정답 수가 가장 많이 맞춘 정답 수와 같다면 answer에 인덱스+1(수포자 번호) 추가       
+    answer = []
+    for i in range(len(ans)):
+        if ans[i] == max(ans): 
+            answer.append(i+1)
+    
+    return answer
