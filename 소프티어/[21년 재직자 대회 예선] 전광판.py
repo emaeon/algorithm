6266967d95f239ac1f87a06257c82c656baa7c4d
emaeon <gun9809@naver.com>
2024-12-07 18:35:53 +0900

    전광판 신호리스트 주의

---
+++ 소프티어/[21년 재직자 대회 예선] 전광판.py
@@ -0,0 +1,66 @@
+import sys
+input = sys.stdin.readline
+
+lst = [[1,1,1,0,1,1,1],
+ [0,0,1,0,0,1,0],[0,1,1,1,1,0,1],[0,1,1,1,0,1,1],
+ [1,0,1,1,0,1,0],[1,1,0,1,0,1,1],[1,1,0,1,1,1,1],
+ [1,1,1,0,0,1,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]]
+
+t = int(input().rstrip())
+for _ in range(t) :
+    a,b = map(str,input().split())
+    lst_a,lst_b = list(a), list(b)
+    cnt = 0
+
+    if len(lst_a) < len(lst_b) :
+        temp = len(lst_b) - len(lst_a)
+        while temp:  # 우선 바뀔숫자가 더 많다면 그만큼 b를 제거
+            num = lst_b.pop(0)
+            cnt += lst[int(num)].count(1)
+            temp -= 1
+
+        for i in range(len(lst_a)):
+
+            before = lst[int(lst_a[i])]
+            after = lst[int(lst_b[i])]
+
+            for j in range(7):
+                if before[j] != after[j]:
+                    cnt += 1
+                else:
+                    pass
+        print(cnt)
+
+    elif len(lst_a) > len(lst_b):
+        temp = len(lst_a) - len(lst_b)
+
+        while temp:  # 우선 바뀔숫자가 더 많다면 그만큼 a를 제거
+            num = lst_a.pop(0)
+            cnt += lst[int(num)].count(1)
+            temp -= 1
+
+        for i in range(len(lst_a)):
+
+            before = lst[int(lst_a[i])]
+            after = lst[int(lst_b[i])]
+
+            for j in range(7):
+                if before[j] != after[j]:
+                    cnt += 1
+                else:
+                    pass
+        print(cnt)
+
+    else:
+
+        for i in range(len(lst_a)):
+
+            before = lst[int(lst_a[i])]
+            after = lst[int(lst_b[i])]
+
+            for j in range(7):
+                if before[j] != after[j]:
+                    cnt += 1
+                else:
+                    pass
+        print(cnt)
